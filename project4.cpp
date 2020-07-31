b865dbca8ad8e34bf287334cc575a8dbb498c958
Rattana548 <68465498+Rattana548@users.noreply.github.com>
2020-07-31 17:10:49 +0700

    Add files via upload

---
+++ project4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+void oklol(float,float,float,float,float);
+
+int main() {
+	short quizz1, quizz2, quizz3,mid,fin;
+	cout << "Enter first quizz (10) : ";
+	cin >> quizz1;
+	cout << "Enter second quizz (10) : ";
+	cin >> quizz2;
+	cout << "Enter third quizz (10) : ";
+	cin >> quizz3;
+	cout << "============MID-TERM==============" << endl;
+	cout << "Enter mid term (40) : ";
+	cin >> mid;
+	cout << "==============FINAL===============" << endl;
+	cout << "Enter final (50) : ";
+	cin >> fin;
+	oklol(quizz1,quizz2,quizz3,mid,fin);
+
+
+	system("pause");
+
+	return 0;
+}
+void oklol(float qui1,float qui2,float qui3 , float mid, float fin) {
+	float addqui = (qui1 + qui2 + qui3) / 3;
+	cout << "Quizz total : " << addqui << endl << "Mid term : " << mid << endl << "Final : " << fin << endl;
+	float total = addqui + (mid + fin);
+	cout << "Total : " << total << endl;
+	cout << "Your score is : " << ((total >= 50) ? "pass" : "Not passed") << endl;
+}
